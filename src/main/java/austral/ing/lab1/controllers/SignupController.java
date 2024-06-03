291b265146cba2d4a13527fa8807864dde8fbcf7
zartf <tomas.alcazar@ing.austral.edu.ar>
2024-06-03 14:52:53 -0300

    fix some feedback of the 1st demo

--- src/main/java/austral/ing/lab1/controllers/SignupController.java
+++ src/main/java/austral/ing/lab1/controllers/SignupController.java
@@ -4,64 +4,56 @@ import austral.ing.lab1.model.Institution;
 import austral.ing.lab1.model.User;
 import austral.ing.lab1.repository.Institutions;
 import austral.ing.lab1.repository.Users;
-import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
-import java.util.Map;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import spark.Request;
-import spark.Response;
-import spark.Route;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 public class SignupController {
 
   private static final Gson gson = new Gson();
   private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("test");
+  private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
-  public static Route handleSignupParticipant = (Request request, Response response) -> {
+  public static Route handleSignupUser = (Request request, Response response) -> {
     String body = request.body();
-    Map<String, String> formData = gson.fromJson(body, new TypeToken<Map<String, String>>() {}.getType());
+    User newUser = gson.fromJson(body, User.class);
 
-    if (formData.get("email").trim().isEmpty() || formData.get("password").trim().isEmpty() ||
-            formData.get("firstname").trim().isEmpty() || formData.get("lastname").trim().isEmpty()) {
-      response.status(400);
-      return "{\"error\": \"Missing or empty fields\"}";
-    }
-
-    String email = formData.get("email");
-    if (!isValidEmail(email)) {
+    if (!isValidEmail(newUser.getEmail())) {
       response.status(400);
       return "{\"error\": \"Invalid email format\"}";
     }
 
     EntityManager entityManager = entityManagerFactory.createEntityManager();
     Users users = new Users(entityManager);
+    Institutions institutions = new Institutions(entityManager);
     EntityTransaction tx = entityManager.getTransaction();
+
     try {
       tx.begin();
 
-      if(users.findByEmail(email).isPresent()){
-        response.status(409);
-        return "{\"error\": \"User already exists\"}";
+      if (emailExists(newUser.getEmail(), users, institutions)) {
+        response.status(400);
+        return "{\"error\": \"Email already in use\"}";
       }
 
-      String password = formData.get("password");
-      String firstname = formData.get("firstname");
-      String lastname = formData.get("lastname");
-
-      User user = User.create(email)
-              .password(password).firstName(firstname).lastName(lastname)
-              .build();
-      User persistedUser = users.persist(user);
+      users.persist(newUser);
       tx.commit();
+
       response.type("application/json");
-      return gson.toJson(Map.of("message", "User signed up successfully"));
+      return gson.toJson(Map.of("message", "User registered successfully"));
     } catch (Exception e) {
       if (tx.isActive()) {
         tx.rollback();
       }
+      e.printStackTrace();
       response.status(500);
       return "{\"error\": \"An error occurred while registering the user\"}";
     } finally {
@@ -71,57 +63,48 @@ public class SignupController {
 
   public static Route handleSignupInstitution = (Request request, Response response) -> {
     String body = request.body();
-    Map<String, String> formData = gson.fromJson(body, new TypeToken<Map<String, String>>() {}.getType());
+    Institution newInstitution = gson.fromJson(body, Institution.class);
 
-    if (formData.get("email").trim().isEmpty() || formData.get("password").trim().isEmpty() ||
-            formData.get("institutionalName").trim().isEmpty() || formData.get("credential").trim().isEmpty()) {
-      response.status(400);
-      return "{\"error\": \"Missing or empty fields\"}";
-    }
-
-    String email = formData.get("email");
-    if (!isValidEmail(email)) {
+    if (!isValidEmail(newInstitution.getEmail())) {
       response.status(400);
       return "{\"error\": \"Invalid email format\"}";
     }
 
     EntityManager entityManager = entityManagerFactory.createEntityManager();
     Institutions institutions = new Institutions(entityManager);
+    Users users = new Users(entityManager);
     EntityTransaction tx = entityManager.getTransaction();
+
     try {
       tx.begin();
 
-      if(institutions.findByEmail(email).isPresent()){
-        response.status(409);
-        return "{\"error\": \"Institution already exists\"}";
+      if (emailExists(newInstitution.getEmail(), users, institutions)) {
+        response.status(400);
+        return "{\"error\": \"Email already in use\"}";
       }
 
-      String password = formData.get("password");
-      String institutionalName = formData.get("institutionalName");
-      String credential = formData.get("credential");
-
-      Institution institution = Institution.create(email)
-              .password(password).institutionalName(institutionalName).credential(credential).build();
-      Institution persistedInstitution = institutions.persist(institution);
-
+      institutions.persist(newInstitution);
       tx.commit();
+
       response.type("application/json");
-      return gson.toJson(Map.of("message", "Institution signed up successfully"));
+      return gson.toJson(Map.of("message", "Institution registered successfully"));
     } catch (Exception e) {
       if (tx.isActive()) {
         tx.rollback();
       }
+      e.printStackTrace();
       response.status(500);
-      return "{\"error\": \"An error occurred while registering the Institution\"}";
+      return "{\"error\": \"An error occurred while registering the institution\"}";
     } finally {
       entityManager.close();
     }
   };
 
+  private static boolean emailExists(String email, Users users, Institutions institutions) {
+    return users.findByEmail(email).isPresent() || institutions.findByEmail(email).isPresent();
+  }
+
   private static boolean isValidEmail(String email) {
-    // Implement email validation logic here
-    // For example, you can use regular expressions or a library like Apache Commons Validator
-    // Here's a basic example:
-    return email.matches("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}");
+    return EMAIL_PATTERN.matcher(email).find();
   }
 }
--- src/main/js/front-edu-gateway/src/components/oportunidades/institucion/AgregarOportunidad.jsx
+++ src/main/js/front-edu-gateway/src/components/oportunidades/institucion/AgregarOportunidad.jsx
@@ -2,55 +2,60 @@ import { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Form from "react-bootstrap/Form";
-import {useNavigate} from "react-router-dom";
-import {loginUser} from "../../../services/auth";
+import { useNavigate } from "react-router-dom";
 import Button from "react-bootstrap/Button";
-import {addOpportunity} from "../../../services/Api";
+import { addOpportunity } from "../../../services/Api";
 
-export function AgregarOportunidad({actualizarOportunidades}){
+export function AgregarOportunidad({ actualizarOportunidades }) {
 
     const [addSuccess, setAddSuccess] = useState(false); // true o false
     const [addError, setAddError] = useState(''); // indica error en caso de error
 
-
     // Controles del modal
-    const[visible, setVisible] = useState(false);
+    const [visible, setVisible] = useState(false);
     const abrir = () => setVisible(true);
     const cerrar = () => {
-        setVisible(false)
-        setAddError('')
-        setAddSuccess(false)
+        setVisible(false);
+        setAddError('');
+        setAddSuccess(false);
     }
 
     // Objeto navegación
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
     // Controles de respuesta de form
     const [name, setName] = useState('');
     const [language, setLanguage] = useState('');
-    const [educationalLevel, setEducationalLevel] = useState('')
-    const [modality, setModality] = useState('')
-    const [city, setCity] = useState('')
-    const [category, setCategory] = useState('')
-    const [capacity, setCapacity] = useState(0)
-
+    const [educationalLevel, setEducationalLevel] = useState('');
+    const [modality, setModality] = useState('');
+    const [city, setCity] = useState('');
+    const [category, setCategory] = useState('');
+    const [capacity, setCapacity] = useState(0);
 
     // Función que se ejecuta al presionar botón de envío, y que llama a función addOpportunity en Api.js
-    async function enviarForm(){
-        try{
-            const opportunityData = {name:name, language:language, educationalLevel:educationalLevel, modality:modality, city:city, category:category, capacity:capacity}
-            await addOpportunity(opportunityData)
-            setAddSuccess(true)
-            setAddError('')
-            actualizarOportunidades()
-        }catch (error){
-            console.error("Error al añadir oportunidad: ", error)
-            setAddSuccess(false)
-            setAddError("Error al agregar oportunidad")
+    async function enviarForm() {
+        try {
+            const opportunityData = {
+                name: name,
+                language: language,
+                educationalLevel: educationalLevel,
+                modality: modality,
+                city: city,
+                category: category,
+                capacity: capacity
+            }
+            await addOpportunity(opportunityData);
+            setAddSuccess(true);
+            setAddError('');
+            actualizarOportunidades();
+        } catch (error) {
+            console.error("Error al añadir oportunidad: ", error);
+            setAddSuccess(false);
+            setAddError("Error al agregar oportunidad");
         }
     }
 
-    return(
+    return (
         <>
             <Button variant="dark" onClick={abrir}>Agregar oportunidad</Button>
 
@@ -69,34 +74,33 @@ export function AgregarOportunidad({actualizarOportunidades}){
                     </Form.Select>
                     <div className="form-oportunidad">
                         <FloatingLabel controlId="floatingName" label="Nombre" className="mb-3">
-                            <Form.Control type="text" placeholder="Nombre de la oportunidad" onChange={(event)=>{setName(event.target.value)}}/>
+                            <Form.Control type="text" placeholder="Nombre de la oportunidad" onChange={(event) => { setName(event.target.value) }} />
                         </FloatingLabel>
                         <FloatingLabel controlId="floatingLanguage" label="Idioma" className="mb-3">
-                            <Form.Control type="text" placeholder="Idioma de la oportunidad" onChange={(event)=>{setLanguage(event.target.value)}}/>
+                            <Form.Control type="text" placeholder="Idioma de la oportunidad" onChange={(event) => { setLanguage(event.target.value) }} />
                         </FloatingLabel>
                         <FloatingLabel controlId="floatingEducationalLevel" label="Nivel educativo" className="mb-3">
-                            <Form.Control type="text" placeholder="Nivel educativo requerido" onChange={(event)=>{setEducationalLevel(event.target.value)}}/>
+                            <Form.Control type="text" placeholder="Nivel educativo requerido" onChange={(event) => { setEducationalLevel(event.target.value) }} />
                         </FloatingLabel>
                         <FloatingLabel controlId="floatingModality" label="Modalidad" className="mb-3">
-                            <Form.Control type="text" placeholder="Modalidad" onChange={(event)=>{setModality(event.target.value)}}/>
+                            <Form.Control type="text" placeholder="Modalidad" onChange={(event) => { setModality(event.target.value) }} />
                         </FloatingLabel>
                         <FloatingLabel controlId="floatingCity" label="Ciudad" className="mb-3">
-                            <Form.Control type="text" placeholder="Ciudad" onChange={(event)=>{setCity(event.target.value)}}/>
+                            <Form.Control type="text" placeholder="Ciudad" onChange={(event) => { setCity(event.target.value) }} />
                         </FloatingLabel>
                         <FloatingLabel controlId="floatingCapacity" label="Capacidad" className="mb-3">
-                            <Form.Control type="number" placeholder="Capacidad" onChange={(event)=>{setCapacity(event.target.value)}}/>
+                            <Form.Control type="number" placeholder="Capacidad" min="0" onChange={(event) => { setCapacity(parseInt(event.target.value)) }} />
                         </FloatingLabel>
                     </div>
                 </Modal.Body>
 
-
                 <Modal.Footer>
-                    {addError!=='' && <p style={{ color: 'red', marginTop: 10 }}>{addError}</p>}
+                    {addError !== '' && <p style={{ color: 'red', marginTop: 10 }}>{addError}</p>}
                     {addSuccess && <p style={{ marginTop: 10 }}>Oportunidad agregada con éxito.</p>}
                     <Button variant="success" onClick={enviarForm}>Confirmar</Button>
                     <Button variant="dark" onClick={cerrar}>Cerrar</Button>
                 </Modal.Footer>
-                </Modal>
+            </Modal>
         </>
     )
 }
