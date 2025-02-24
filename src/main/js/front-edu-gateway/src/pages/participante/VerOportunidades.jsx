6a6191c51509d80d4cef76beb1dedd62f60cf2d8
zartf <tomas.alcazar@ing.austral.edu.ar>
2025-02-24 19:56:18 -0300

    feat: make a dropdown for category filter

--- src/main/js/front-edu-gateway/src/pages/participante/VerOportunidades.jsx
+++ src/main/js/front-edu-gateway/src/pages/participante/VerOportunidades.jsx
@@ -1,10 +1,10 @@
-import {getToken, getUserType} from "../../services/storage";
-import {mostrarAlertaAutenticacion} from "../../components/AlertaAutenticacion";
-import {NavbarParticipante} from "../../components/navbar/NavbarParticipante";
-import {useNavigate} from "react-router";
-import {ContenedorOportunidadesParticipante} from "../../components/oportunidades/participante/ContenedorOportunidadesParticipante";
-import {Dropdown, Form} from "react-bootstrap";
-import {useEffect, useState} from "react";
+import { getToken, getUserType } from "../../services/storage";
+import { mostrarAlertaAutenticacion } from "../../components/AlertaAutenticacion";
+import { NavbarParticipante } from "../../components/navbar/NavbarParticipante";
+import { useNavigate } from "react-router";
+import { ContenedorOportunidadesParticipante } from "../../components/oportunidades/participante/ContenedorOportunidadesParticipante";
+import { Dropdown, Form } from "react-bootstrap";
+import { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import {
     getFollowedInstitutions,
@@ -21,13 +21,14 @@ const filterOptions = [
     "Nombre",
 ];
 
-export function VerOportunidades ({
+const categoryOptions = ["Voluntariado", "Curso", "Programa", "Evento"];
 
-                                  }){
+export function VerOportunidades (){
 
     const navigate = useNavigate();
-    const [selectedFilterOption, setSelectedFilterOption] = useState(filterOptions[0])
-    const [searchValue, setSearchValue] = useState('')
+    const [selectedFilterOption, setSelectedFilterOption] = useState(filterOptions[0]);
+    const [selectedCategory, setSelectedCategory] = useState(categoryOptions[0]);
+    const [searchValue, setSearchValue] = useState('');
     const [oportunidades, setOportunidades] = useState([]);
 
     useEffect(() => {
@@ -42,26 +43,30 @@ export function VerOportunidades ({
         fetchOpportunities();
     }, []);
 
-    if (!getToken() || getUserType()!=="participant"){
+    if (!getToken() || getUserType() !== "participant"){
         return (
             <>
                 {mostrarAlertaAutenticacion(navigate, "/")}
             </>
-        )
+        );
     }
 
     const handleFilterOptionChange = async (option) => {
-        setSelectedFilterOption(option)
-        setSearchValue('')
+        setSelectedFilterOption(option);
+        setSearchValue('');
         if (option === 'Todos') {
             const response1 = await getOpportunities();
-            setOportunidades(response1  ? response1 : []);
+            setOportunidades(response1 ? response1 : []);
         }
         if (option === 'Seguidos') {
             const response2 = await getFollowedInstitutions();
             setOportunidades(response2 ? response2 : []);
         }
-    }
+    };
+
+    const handleCategoryChange = (category) => {
+        setSelectedCategory(category);
+    };
 
     const handleClick = async () => {
         switch (selectedFilterOption) {
@@ -74,7 +79,7 @@ export function VerOportunidades ({
                 setOportunidades(response2 ? response2 : []);
                 break;
             case 'Categoria':
-                const response3 = await getOpportunitiesByCategory(searchValue);
+                const response3 = await getOpportunitiesByCategory(selectedCategory);
                 setOportunidades(response3 ? response3 : []);
                 break;
             case 'Institucion':
@@ -86,11 +91,11 @@ export function VerOportunidades ({
                 setOportunidades(response5 ? response5 : []);
                 break;
         }
-    }
+    };
 
     return(
         <>
-            <NavbarParticipante></NavbarParticipante>
+            <NavbarParticipante />
             <div className="contenido-pagina-oportunidades">
                 <h1>Ver oportunidades</h1>
                 <div style={{display: 'flex', gap: '16px', alignItems: 'center'}}>
@@ -99,27 +104,44 @@ export function VerOportunidades ({
                             {selectedFilterOption}
                         </Dropdown.Toggle>
                         <Dropdown.Menu>
-                            {filterOptions.map((option, index) =>
+                            {filterOptions.map((option, index) => (
                                 <Dropdown.Item
                                     onClick={() => handleFilterOptionChange(option)}
                                     key={index}
                                 >
                                     {option}
                                 </Dropdown.Item>
-                            )}
+                            ))}
                         </Dropdown.Menu>
                     </Dropdown>
-                    {selectedFilterOption !== 'Todos' && selectedFilterOption !== 'Seguidos' &&
+                    {selectedFilterOption === 'Categoria' && (
+                        <Dropdown>
+                            <Dropdown.Toggle variant="secondary" id="dropdown-category">
+                                {selectedCategory}
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                {categoryOptions.map((category, index) => (
+                                    <Dropdown.Item
+                                        onClick={() => handleCategoryChange(category)}
+                                        key={index}
+                                    >
+                                        {category}
+                                    </Dropdown.Item>
+                                ))}
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    )}
+                    {selectedFilterOption !== 'Todos' && selectedFilterOption !== 'Seguidos' && selectedFilterOption !== 'Categoria' && (
                         <Form.Group>
                             <Form.Control
                                 value={searchValue}
                                 onChange={(e) => setSearchValue(e.target.value)}
                                 placeholder={`Buscar por ${selectedFilterOption}`}
-                                rows={1}/>
+                            />
                         </Form.Group>
-                    }
+                    )}
                 </div>
-                {selectedFilterOption !== 'Todos' && selectedFilterOption !== 'Seguidos' &&
+                {selectedFilterOption !== 'Todos' && selectedFilterOption !== 'Seguidos' && (
                     <div style={{marginTop: '16px'}}>
                         <Button
                             onClick={handleClick}
@@ -128,11 +150,11 @@ export function VerOportunidades ({
                             Buscar
                         </Button>
                     </div>
-                }
+                )}
                 <ContenedorOportunidadesParticipante
                     oportunidades={oportunidades}
                 />
             </div>
         </>
-    )
+    );
 }
